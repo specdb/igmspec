12bed7698e690674c1241f7f97d7809a524cd195
J. Xavier Prochaska <xavier@ucolick.org>
2016-09-20 08:14:29 -0700

    zem; privatedb gone

--- igmspec/privatedb/tests/get_zidx.py
+++
@@ -1,9 +0,0 @@
-from linetools import utils as ltu
-coords = [ltu.radec_to_coord(jc) for jc in ['J001605.89+005654.3','J001607.27+005653.1']]
-from igmspec.igmspec import IgmSpec
-from igmspec import cat_utils as icu
-igmsp = IgmSpec()
-# PUT A PDB IN cat_utils
-zem = icu.zem_from_radec([icoord.ra.value for icoord in coords],
-                         [icoord.dec.value for icoord in coords], igmsp.idb.hdf)
-# 161121, 161130
