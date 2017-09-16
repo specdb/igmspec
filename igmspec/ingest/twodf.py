d52fa36ad95edf71048a89ace59346bdd7562771
profxj <xavier@ucolick.org>
2017-09-16 15:23:22 -0700

    environmental variable

--- igmspec/ingest/twodf.py
+++ igmspec/ingest/twodf.py
@@ -251,7 +251,6 @@ def hdf5_adddata(hdf, sname, meta, debug=False, chk_meta_only=False):
             pdb.set_trace()
         hdf[sname]['meta'] = meta
     else:
-        pdb.set_trace()
         raise ValueError("meta file failed")
     #
     # References
