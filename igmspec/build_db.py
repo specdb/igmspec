15b4d7e3f16b50200ecd52b3c0046063ea9f9c21
profxj <xavier@ucolick.org>
2018-02-27 13:17:54 -0800

    running but crashing..

--- igmspec/build_db.py
+++ igmspec/build_db.py
@@ -24,6 +24,7 @@ from igmspec.ingest import hst_cooksey as hst_c
 from igmspec.ingest import cos_dwarfs
 from igmspec.ingest import musodla
 from igmspec.ingest import uves_dall
+from igmspec.ingest import boss_dr14
 
 from astropy.table import Table, vstack, Column
 from astropy import units as u
@@ -245,6 +246,165 @@ def ver02(test=False, skip_copy=False, publisher='J.X. Prochaska', clobber=False
     print("Update DB info in specdb.defs.dbase_info !!")
 
 
+def ver03(test=False, skip_copy=False, publisher='J.X. Prochaska', clobber=False,
+          version='v03', out_path=None):
+    """ Build version 3.X
+    Reads several previous datasets from v1.X
+    Remakes the maindb using BOSS DR14 as the main driver
+
+    v3.0 will be BOSS DR14 only to speed up generation of the rest
+
+    Parameters
+    ----------
+    test : bool, optional
+      Run test only
+    skip_copy : bool, optional
+      Skip copying the data from v01
+
+    Returns
+    -------
+    """
+    import os
+    from specdb.specdb import IgmSpec
+    # Read v02
+    v02file = os.getenv('SPECDB')+'/IGMspec_DB_v02.1.hdf5'
+    #v01file_debug = igmspec.__path__[0]+'/tests/files/IGMspec_DB_v01_debug.hdf5'
+    print("Loading v02")
+    igmsp_v02 = IgmSpec(db_file=v02file)
+    v02hdf = igmsp_v02.hdf
+    #maindb = igmsp_v02.cat.copy()
+
+    # Start new file
+    if out_path is None:
+        out_path = '/scratch/IGMSpec/'
+    outfil = out_path + 'IGMspec_DB_{:s}.hdf5'.format(version)
+    # Clobber?
+    if not chk_clobber(outfil, clobber=clobber):
+        return
+    # Begin
+    hdf = h5py.File(outfil,'w')
+
+    # Set/Check keys (and set idkey internally for other checks)
+    idkey = 'IGM_ID'
+    maindb, tkeys = sdbbu.start_maindb(idkey)
+    group_dict = {}
+
+    # BOSS DR14
+    gname = 'BOSS_DR14'
+    print("Working on group: {:s}".format(gname))
+    # Meta
+    new_groups = get_build_groups('v03')
+    meta = new_groups[gname].grab_meta()
+    pair_groups = []
+
+    # Survey flag
+    flag_g = sdbbu.add_to_group_dict(gname, group_dict, skip_for_debug=True)
+    # IDs
+    maindb = sdbbu.add_ids(maindb, meta, flag_g, tkeys, idkey,
+                           first=(flag_g==1), close_pairs=(gname in pair_groups),
+                           debug=False)
+    # Spectra
+    new_groups[gname].hdf5_adddata(hdf, gname, meta)
+    new_groups[gname].add_ssa(hdf, gname)
+
+    pdb.set_trace()
+
+    # Copy over the old stuff
+    redo_groups = []#'HD-LLS_DR1']
+    skip_groups = ['BOSS_DR12']# 'SDSS_DR7'] #warnings.warn("NEED TO PUT BACK SDSS AND BOSS!")
+    skip_copy = False
+    if (not test) and (not skip_copy):
+        old_groups = get_build_groups('v01')
+        for key in v01hdf.keys():
+            if key in ['catalog','quasars']+redo_groups+skip_groups:
+                continue
+            else:
+                #v01hdf.copy(key, hdf)  # ONE STOP SHOPPING
+                grp = hdf.create_group(key)
+                # Copy spectra
+                v01hdf.copy(key+'/spec', hdf[key])
+                # Modify v01 meta and add
+                if key == 'BOSS_DR12':
+                    meta = boss.add_coflag(v01hdf)
+                else:
+                    meta = Table(v01hdf[key+'/meta'].value)
+                meta.rename_column('GRATING', 'DISPERSER')
+                hdf[key+'/meta'] = meta
+                for akey in v01hdf[key+'/meta'].attrs.keys():
+                    hdf[key+'/meta'].attrs[akey] = v01hdf[key+'/meta'].attrs[akey]
+                # SSA info
+                old_groups[key].add_ssa(hdf, key)
+    skip_myers = False
+    if skip_myers:
+        warnings.warn("NEED TO INCLUDE MYERS!")
+    else:
+        myers.add_to_hdf(hdf)
+
+    # Setup groups
+    old_groups = get_build_groups('v01')
+    pair_groups = []
+    group_dict = igmsp_v01.qcat.group_dict
+
+
+    # Loop over the old groups to update (as needed)
+    new_IDs = False
+    for gname in redo_groups:
+        print("Working to replace meta/spec for group: {:s}".format(gname))
+        # Meta
+        meta = old_groups[gname].grab_meta()
+        # Group flag
+        flag_g = group_dict[gname]
+        # IDs
+        if new_IDs:
+            pdb.set_trace()  # NOT READY FOR THIS
+            #maindb = sdbbu.add_ids(maindb, meta, flag_g, tkeys, idkey,
+            #                   first=(flag_g==1), close_pairs=(gname in pair_groups))
+        else:
+            _, _, ids = sdbbu.set_new_ids(maindb, meta, idkey)
+        # Spectra
+        old_groups[gname].hdf5_adddata(hdf, gname, meta)
+        old_groups[gname].add_ssa(hdf, gname)
+
+    meta_only = False
+    new_groups = get_build_groups(version)
+    # Loop over the new groups
+    for gname in new_groups:
+        print("Working on group: {:s}".format(gname))
+        # Meta
+        meta = new_groups[gname].grab_meta()
+        # Survey flag
+        flag_g = sdbbu.add_to_group_dict(gname, group_dict, skip_for_debug=True)
+        # IDs
+        debug= False
+        #if gname == 'XQ-100':
+        #    debug = True
+        maindb = sdbbu.add_ids(maindb, meta, flag_g, tkeys, idkey,
+                               first=(flag_g==1), close_pairs=(gname in pair_groups),
+                               debug=debug)
+        # Spectra
+        if not meta_only:
+            new_groups[gname].hdf5_adddata(hdf, gname, meta)
+            new_groups[gname].add_ssa(hdf, gname)
+
+    # Check for duplicates -- There is 1 pair in SDSS (i.e. 2 duplicates)
+    if not sdbbu.chk_for_duplicates(maindb, dup_lim=2):
+        raise ValueError("Failed duplicates")
+
+    # Check stacking
+    if not sdbbu.chk_vstack(hdf):
+        print("Meta data will not stack using specdb.utils.clean_vstack")
+        print("Proceed to write at your own risk..")
+        pdb.set_trace()
+
+    # Finish
+    zpri = v01hdf['catalog'].attrs['Z_PRIORITY']
+    sdbbu.write_hdf(hdf, str('igmspec'), maindb, zpri,
+                    group_dict, version, Publisher=str(publisher))
+
+    print("Wrote {:s} DB file".format(outfil))
+    print("Update DB info in specdb.defs.dbase_info !!")
+
+
 def chk_clobber(outfil, clobber=False):
     """ Simple clobber check
     outfil : str
@@ -282,7 +442,6 @@ def get_build_groups(version):
         groups['HD-LLS_DR1'] = hdlls
         groups['GGG'] = ggg
     elif version[0:3] == 'v02':
-        groups = OrderedDict()
         groups['HST_z2'] = hst_z2       # O'Meara et al. 2011
         groups['XQ-100'] = xq100        # Lopez et al. 2016
         groups['HDLA100'] = hdla100     # Neeleman et al. 2013
@@ -294,6 +453,8 @@ def get_build_groups(version):
         groups['MUSoDLA'] = musodla     # Jorgensen et al. 2013
         groups['UVES_Dall'] = uves_dall # Dall'Aglio et al. 2008
         groups['UVpSM4'] = hst_c        # Cooksey et al. 2010, 2011
+    elif version[0:3] == 'v03':
+        groups['BOSS_DR14'] = boss_dr14 # Paris et al.
     else:
         raise IOError("Not ready for this version")
     # Return
